8a14ecba51bea638b3167893a7ed3682b81f8a84
Jan Möller <drag-on@gmx.de>
2025-09-10 20:19:09 +0200

    Change text parser factory to take a variadic number of arguments

--- src/pyforma/_parser/text.py
+++ src/pyforma/_parser/text.py
@@ -3,14 +3,13 @@ from .not_in import not_in
 from .parser import Parser
 
 
-def text(open_comment: str, open_expression: str) -> Parser[str]:
+def text(*end_strs: str) -> Parser[str]:
     """Creates a parser of unstructured text
 
     Args:
-        open_comment: comment indicator (ends unstructured text)
-        open_expression: expression indicator (ends unstructured text)
+        end_strs: syntax indicators that end unstructured text
 
     Returns:
         A parser for unstructured text
     """
-    return not_in(literal(open_comment), literal(open_expression))
+    return not_in(*(literal(s) for s in end_strs))
